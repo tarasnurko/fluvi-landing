5d4fe6a902e834dfd4e576946843c8a1150fec41
Taras Nurko <tarasnurko84@gmail.com>
2022-11-18 19:35:11 +0200

    project initialzed, added home-head component

---
+++ epic/home-head/component.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import React from "react";
+
+const Component = () => {
+  return (
+    <Head>
+      <title>Home</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+  );
+};
+
+export default Component;
